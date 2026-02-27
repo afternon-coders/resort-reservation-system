031d7ed2085085cd2fee0249211c08a2a97da56f
Warren Afable <afablewarren2004@gmail.com>
2026-02-27 15:17:02 +0800

    add design to gallery and photos

--- gallery.php
+++ gallery.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amenities - Luxury Resort</title>
     <link rel="stylesheet" href="static/css/style.css">
+
 </head>
 <body>
 
@@ -16,6 +17,24 @@
 
         </div>
     </section>
+
+    <section class="gallery">
+        <div class="gallery-left">
+            <img src="static/images/lapaseo_bar.jpg" alt="Barr Mont Le Paseo Resort">
+            <div class="caption">Barr Mont Le Paseo Resort</div>
+        </div>
+        <div class="gallery-right">
+            <div class="gallery-item">
+                <img src="static/images/sea_view.jpg" alt="Ocean view from the resort">
+                <div class="caption">Ocean View from the Resort</div>
+            </div>
+            <div class="gallery-item">
+                <img src="static/images/cottage.jpg" alt="Overwater Suite">
+                <div class="caption">Resort Cottage</div>
+            </div>
+        </div>
+    </section>
+
 
 
 
